023e971d6052e31f7c82264889401a3d7caeee98
伯飞 <bofei.zq@taobao.com>
2016-08-05 21:38:18 +0800

    update

--- src/user/routes/login.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-    path: 'login',
-
-    getComponents(nextState, callback) {
-    require.ensure([], function (require) {
-      callback(null, require('../components/login.js'))
-    })
-    }
-  }
