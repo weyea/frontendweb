52526b757a59cfcd09de450fffb97ed9a43d7b84
伯飞 <bofei.zq@taobao.com>
2017-08-07 17:17:00 +0800

    udpate

--- src/designer/index.js
+++ src/designer/index.js
@@ -1,7 +1,9 @@
+
+var login = require("../common/login");
 module.exports = {
 
     path: 'designer/:type/:appId',
-
+    onEnter:login.checkLoginRouter,
     getComponents(nextState, callback) {
     require.ensure([], function (require) {
       callback(null, require('./components'))
