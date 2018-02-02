3374fe2f5c53533dac22f8013e6070893a4143e7
伯飞 <bofei.zq@taobao.com>
2018-02-02 17:52:38 +0800

    update

--- src/preview/components/index.js
+++ src/preview/components/index.js
@@ -20,6 +20,7 @@ module.exports =  React.createClass({
                   location.href = data.loginURL;
                   return;
               }
+              return;
               var url = "/designer/app/" + data.id
               location.href = url;
 
